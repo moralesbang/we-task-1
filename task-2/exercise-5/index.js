067554219a406ebbe6b93a46e73edc1e52f501dd
milo <milo@pop-os.localdomain>
2020-09-24 15:31:53 -0500

    resolve exercise #5

---
+++ task-2/exercise-5/index.js
@@ -0,0 +1,13 @@
+const getOccurrence = (list, element) =>
+  list.filter((value) => value === element).length;
+
+// Returns a new array with no-unique elements in an array
+const removeUnique = (elements) =>
+  elements.reduce((result, value) => {
+    if (getOccurrence(elements, value) > 1) {
+      return [...result, value];
+    }
+    return result;
+  }, []);
+
+console.log(removeUnique([1, 2, 3, 1, 3])); // [1, 3, 1, 3]
