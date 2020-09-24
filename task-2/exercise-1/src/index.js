b802132798ce2a9c3fa234a11bb2cc52848c8c58
milo <milo@pop-os.localdomain>
2020-09-24 14:43:15 -0500

    refactor code

--- task-2/exercise-1/src/index.js
+++ task-2/exercise-1/src/index.js
@@ -2,13 +2,13 @@ const ABC_REGEX = /([a-z])(?!.*\1)/g;
 
 const getElement = (id) => document.getElementById(id);
 
+// Works for english language
 const isPangram = (str) => {
   return str.match(ABC_REGEX).length === 26;
 };
 
 getElement("check-btn").addEventListener("click", () => {
   const str = getElement("input").value;
-  console.log(str);
   const resultElement = getElement("result");
   resultElement.innerHTML = isPangram(str);
 });
